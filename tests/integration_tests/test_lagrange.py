61c830a821834fe7f2a32edc878b8ca90709bb58
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2024-10-16 16:59:50 +0100

    Adapt Lagrange to new design

--- tests/integration_tests/test_lagrange.py
+++ tests/integration_tests/test_lagrange.py
@@ -5,8 +5,7 @@ import pytest
 import mantid
 from abins.instruments.lagrangeinstrument import LagrangeInstrument
 
-from resolution_functions.instruments.indirect_instruments import Lagrange
-
+from resolution_functions.instruments.instrument import Instrument
 
 WAVENUMBER_TO_MEV = 0.12398419843320028
 MEV_TO_WAVENUMBER = 1 / WAVENUMBER_TO_MEV
@@ -15,13 +14,13 @@ LAGRANGE_SETTINGS = ['Cu(220)', 'Cu(331)', 'Si(311)', 'Si(111)']
 
 @pytest.fixture(scope="module")
 def lagrange_rf():
-    return Lagrange.from_default('Lagrange')
+    return Instrument.from_default('Lagrange', 'Lagrange')
 
 
 @pytest.fixture(scope="module", params=LAGRANGE_SETTINGS)
 def lagrange_abins_plus_rf_abins_resolution_function(lagrange_rf, request):
     abins = LagrangeInstrument(setting=request.param + ' (Lagrange)')
-    rf = lagrange_rf.get_resolution_function('AbINS', [request.param])
+    rf = lagrange_rf.get_resolution_function('AbINS', monochromator=request.param)
     return abins, rf
 
 
